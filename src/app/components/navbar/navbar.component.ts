53e2e211c1890c8bb53f73a13194f578c9ee6731
Joe Barrett <joe.barrett@stfc.ac.uk>
2017-08-07 14:40:57 +0100

    Added items to navbar with loop

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,12 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-import {log} from "util";
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent implements OnInit {
+
+  items = [
+    'General',
+    'Field Setup',
+    'Spectral Setup',
+    'Calibration Setup',
+    'Control and Performance',
+    'Technical Justification'
+  ];
 
   constructor() { }
 

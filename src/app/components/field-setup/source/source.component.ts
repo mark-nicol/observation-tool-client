3e42eb916c87f11256b66b9ec4e2e644b5b6f756
JoeBarrett <joe.barrett@stfc.ac.uk>
2017-08-29 16:54:57 +0100

    Adding further dynamics to form

--- src/app/components/field-setup/source/source.component.ts
+++ src/app/components/field-setup/source/source.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'field-source',
@@ -31,21 +31,148 @@ export class SourceComponent implements OnInit {
     'Ephemeris'
   ];
 
-  systems =[
-    'ICRS',
-    'FK5 J2000',
-    'galactic',
-    'eliptic',
-    'horizon',
-    'azel'
-  ];
+  sexagesimalEnabled = true;
+  systemKeys = Object.keys;
+  systems: {
+    [id: string]: {
+      sexagesimalLabels: {
+        latLabel: string,
+        longLabel: string,
+        latPlaceholder: string,
+        longPlaceholder: string
+      },
+      normalLabels: {
+        latLabel: string,
+        longLabel: string,
+        latPlaceholder: string,
+        longPlaceholder: string
+      },
+      tableHeaders: {
+        longHeader: string,
+        latHeader: string
+      }
+
+    }
+  } = {
+    'ICRS': {
+      sexagesimalLabels: {
+        latLabel: 'Dec',
+        longLabel: 'RA',
+        latPlaceholder: '00:00:00.000',
+        longPlaceholder: '00:00:00.000'
+      },
+      normalLabels: {
+        latLabel: 'Dec (deg)',
+        longLabel: 'RA (deg)',
+        latPlaceholder: '0.00000',
+        longPlaceholder: '0.00000'
+      },
+      tableHeaders: {
+        longHeader: 'RA',
+        latHeader: 'Dec'
+      }
+    },
+    'FK5 J2000': {
+      sexagesimalLabels: {
+        latLabel: 'Dec',
+        longLabel: 'RA',
+        latPlaceholder: '00:00:00.000',
+        longPlaceholder: '00:00:00.000'
+      },
+      normalLabels: {
+        latLabel: 'Deg (deg)',
+        longLabel: 'RA (deg)',
+        latPlaceholder: '0.00000',
+        longPlaceholder: '0.00000'
+      },
+      tableHeaders: {
+        longHeader: 'RA',
+        latHeader: 'Dec'
+      }
+    },
+    'galactic': {
+      sexagesimalLabels: {
+        latLabel: '',
+        longLabel: '',
+        latPlaceholder: '',
+        longPlaceholder: ''
+      },
+      normalLabels: {
+        latLabel: 'Lat (deg)',
+        longLabel: 'Lon (deg)',
+        latPlaceholder: '-60.18855219',
+        longPlaceholder: '96.33728304'
+      },
+      tableHeaders: {
+        longHeader: 'Lon',
+        latHeader: 'Lat'
+      }
+    },
+    'eliptic': {
+      sexagesimalLabels: {
+        latLabel: '',
+        longLabel: '',
+        latPlaceholder: '',
+        longPlaceholder: ''
+      },
+      normalLabels: {
+        latLabel: 'Lat (deg)',
+        longLabel: 'Lon (deg)',
+        latPlaceholder: '0.000000',
+        longPlaceholder: '0.000000'
+      },
+      tableHeaders: {
+        longHeader: 'RA',
+        latHeader: 'Deg'
+      }
+    },
+    'horizon': {
+      sexagesimalLabels: {
+        latLabel: '',
+        longLabel: '',
+        latPlaceholder: '',
+        longPlaceholder: ''
+      },
+      normalLabels: {
+        latLabel: 'Alt (deg)',
+        longLabel: 'Az (deg)',
+        latPlaceholder: '0.000000',
+        longPlaceholder: '0.000000'
+      },
+      tableHeaders: {
+        longHeader: 'RA',
+        latHeader: 'Deg'
+      }
+    },
+    'azel': {
+      sexagesimalLabels: {
+        latLabel: '',
+        longLabel: '',
+        latPlaceholder: '',
+        longPlaceholder: ''
+      },
+      normalLabels: {
+        latLabel: 'Alt (deg)',
+        longLabel: 'Az (deg)',
+        latPlaceholder: '0.000000',
+        longPlaceholder: '0.000000'
+      },
+      tableHeaders: {
+        longHeader: 'RA',
+        latHeader: 'Deg'
+      }
+    }
+  };
+  chosenSystem = 'ICRS';
 
   showSourceCoords = true;
+  sexagesimalUnits = false;
 
   latLongSystemChosen = false;
-  chosenSystem:string;
 
-  constructor() { }
+
+  constructor() {
+  }
 
   ngOnInit() {
   }
@@ -54,8 +181,13 @@ export class SourceComponent implements OnInit {
     this.showSourceCoords = !this.showSourceCoords;
   }
 
-  changeSystem(value: string) {
-    this.latLongSystemChosen = !(this.chosenSystem === 'ICRS' || this.chosenSystem === 'FK5 J2000');
+  sexagesimalCheckboxClicked() {
+    this.sexagesimalUnits = !this.sexagesimalUnits;
+    console.log(this.chosenSystem, this.sexagesimalUnits);
+  }
+
+  systemChange() {
+    console.log(this.chosenSystem, this.sexagesimalUnits);
   }
 
 }

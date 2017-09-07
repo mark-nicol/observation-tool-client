6b729852a5557a751dfeeff5a1cd16257852505c
JoeBarrett <joe.barrett@stfc.ac.uk>
2017-09-07 16:42:06 +0100

    Created general component

--- src/app/components/science-goals/science-goal.component.spec.ts
+++ src/app/components/science-goals/science-goal.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ScienceGoalComponent } from './science-goal.component';
+import {RouterTestingModule} from "@angular/router/testing";
 
 describe('ScienceGoalComponent', () => {
   let component: ScienceGoalComponent;
@@ -8,7 +9,8 @@ describe('ScienceGoalComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ScienceGoalComponent ]
+      declarations: [ ScienceGoalComponent ],
+      imports: [RouterTestingModule]
     })
     .compileComponents();
   }));

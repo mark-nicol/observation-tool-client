dab5867c0c871137f2bac0b6cea783833e391db9
JoeBarrett <joe.barrett@stfc.ac.uk>
2017-11-03 12:30:07 +0000

    Commented science goal panel interface

--- src/app/observation-tool/interfaces/panel.ts
+++ src/app/observation-tool/interfaces/panel.ts
@@ -1,4 +1,9 @@
+/**
+ * Interface for a modular panel on a science goal page
+ */
 export interface Panel {
+  /** The title to show in the panel */
   title: string;
+  /** Whether the panel is displayed or not */
   shown: boolean;
 }

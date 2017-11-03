b6c51a95934dc0e0b0bf381bc6c01d822ecf1d1b
JoeBarrett <joe.barrett@stfc.ac.uk>
2017-11-03 12:26:44 +0000

    Commented navbar item interface

--- src/app/observation-tool/interfaces/navbar-item.ts
+++ src/app/observation-tool/interfaces/navbar-item.ts
@@ -1,4 +1,9 @@
+/**
+ * Forces all navbar items to have a title and routing path
+ */
 export interface NavItem {
+  /** The title to show for the item in the navbar */
   title: string;
+  /** The routing path of the item */
   path: string;
 }

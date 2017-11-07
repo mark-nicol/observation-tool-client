67f6178cb22ad8730ab7f926812ac787ec00cd32
JoeBarrett <joe.barrett@stfc.ac.uk>
2017-11-07 12:00:38 +0000

    Updated pi select module to match design

--- src/app/observation-tool/pi-select/pi-select.module.ts
+++ src/app/observation-tool/pi-select/pi-select.module.ts
@@ -1,10 +1,10 @@
 import {NgModule} from '@angular/core';
 import {PiEntryComponent} from './components/pi-entry/pi-entry.component';
 import {PiSearchComponent} from './components/pi-entry/pi-search/pi-search.component';
-import {ProjectInfoComponent} from './components/pi-entry/project-info/project-info.component';
 import {PiSelectComponent} from './components/pi-select/pi-select.component';
 import {RefinePanelComponent} from './components/pi-select/refine-panel/refine-panel.component';
 import {ResultsTableComponent} from './components/pi-select/results-table/results-table.component';
+import {PiSelectRoutingModule} from './pi-select-routing.module';
 import {PrimaryInvestigatorService} from './services/primary-investigator.service';
 
 @NgModule({
@@ -12,13 +12,14 @@ import {PrimaryInvestigatorService} from './services/primary-investigator.servic
     PiEntryComponent,
     PiSearchComponent,
     PiSelectComponent,
-    ProjectInfoComponent,
     RefinePanelComponent,
     ResultsTableComponent,
+    PiSelectRoutingModule,
   ],
-  providers: [PrimaryInvestigatorService],
+  providers: [
+    PrimaryInvestigatorService
+  ],
   exports: [
-    PiEntryComponent,
     PiSelectComponent
   ]
 })

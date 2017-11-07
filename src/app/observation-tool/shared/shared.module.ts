c1d106fca47255710eb6544e355f8d7cd8a31438
JoeBarrett <joe.barrett@stfc.ac.uk>
2017-11-07 11:47:37 +0000

    Updated shared module, added persistence service

--- src/app/observation-tool/shared/shared.module.ts
+++ src/app/observation-tool/shared/shared.module.ts
@@ -1,30 +1,40 @@
+import {CommonModule} from '@angular/common';
 import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {UnitsModule} from '../../units/units.module';
 import {ModularPanelComponent} from './components/modular-panel/modular-panel.component';
+import {SystemSelectorComponent} from './components/system-selector/system-selector.component';
 import {DelayTooltipDirective} from './directives/delay-tooltip.directive';
 import {DegreesPipe} from './pipes/degrees.pipe';
 import {SexagesimalPipe} from './pipes/sexagesimal.pipe';
-import {SystemSelectorComponent} from './components/system-selector/system-selector.component';
-import {CommonModule} from '@angular/common';
-import {UnitsModule} from '../../units/units.module';
+import {PersistenceService} from './services/persistence.service';
 
 @NgModule({
-  imports: [CommonModule, UnitsModule],
+  imports: [
+    CommonModule,
+    FormsModule,
+    UnitsModule,
+  ],
   declarations: [
     ModularPanelComponent,
     DelayTooltipDirective,
     DegreesPipe,
     SexagesimalPipe,
-    SystemSelectorComponent
+    SystemSelectorComponent,
   ],
-  providers: [],
+  providers: [
+    PersistenceService,
+  ],
   exports: [
-    UnitsModule,
     CommonModule,
+    FormsModule,
+    UnitsModule,
     ModularPanelComponent,
+    SystemSelectorComponent,
     DelayTooltipDirective,
     DegreesPipe,
     SexagesimalPipe,
-    SystemSelectorComponent
+    PersistenceService,
   ]
 })
 

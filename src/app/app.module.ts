211457baf4b6c8ad47118818bf8b4852a062d14e
JoeBarrett <joe.barrett@stfc.ac.uk>
2017-11-07 11:53:07 +0000

    Updated app module to match design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,17 @@
-/* Modules */
 import {NgModule} from '@angular/core';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {HttpModule} from '@angular/http'
-import {BrowserModule} from '@angular/platform-browser';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
-import {DragulaModule} from 'ng2-dragula';
 import {AppRoutingModule} from './app-routing.module'
-/* Components */
 import {AppComponent} from './app.component';
-/* Services */
-import {PiSelectionModule} from './observation-tool/pi-selection/pi-selection.module';
-import {SharedModule} from './observation-tool/shared/shared.module';
+import {NavbarModule} from './observation-tool/navbar/navbar.module';
+import {ProjectModule} from './observation-tool/project/project.module';
 
 @NgModule({
   declarations: [
     AppComponent,
   ],
   imports: [
-    SharedModule,
-    PiSelectionModule,
-    BrowserModule,
-    DragulaModule,
-    FormsModule,
-    ReactiveFormsModule,
-    HttpModule,
+    NavbarModule,
+    ProjectModule,
     NgbModule.forRoot(),
     AppRoutingModule,
   ],

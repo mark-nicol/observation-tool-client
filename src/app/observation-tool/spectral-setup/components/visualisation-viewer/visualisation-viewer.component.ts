3b2edb592fc2ef4288f671b412f01b5d6da2f4b3
JoeBarrett <joe.barrett@stfc.ac.uk>
2017-11-13 12:30:26 +0000

    Moved logic into functions and added doc comments

--- src/app/observation-tool/spectral-setup/components/visualisation-viewer/visualisation-viewer.component.ts
+++ src/app/observation-tool/spectral-setup/components/visualisation-viewer/visualisation-viewer.component.ts
@@ -111,6 +111,32 @@ export class VisualisationViewerComponent implements OnInit {
    */
   ngOnInit() {
     this.createData();
+    this.setupSvg();
+    this.setupCharts();
+    this.setupBrushZoom();
+    this.drawRegions();
+    this.drawContextChart();
+    this.drawFocusChart();
+  }
+
+  /**
+   * Creates the data array for the charts to use
+   */
+  createData() {
+    this.sin = [];
+    this.cos = [];
+    this.tan = [];
+    for (let i = 0; i < 1000; i++) {
+      this.sin.push([i, Math.sin(i / 10)]);
+      this.cos.push([i, Math.cos(i / 20)]);
+      this.tan.push([i, Math.tan(i / 10)]);
+    }
+  }
+
+  /**
+   * Sets up heights, widths and adds the svg element to the DOM
+   */
+  setupSvg() {
     const element       = this.chartContainer.nativeElement;
     this.context.width  = element.offsetWidth - this.context.margin.left - this.context.margin.right;
     this.context.height = element.offsetHeight - this.context.margin.top - this.context.margin.bottom;
@@ -121,6 +147,17 @@ export class VisualisationViewerComponent implements OnInit {
       .attr('width', element.offsetWidth)
       .attr('height', element.offsetHeight);
 
+    this.svg.append('defs').append('clipPath')
+      .attr('id', 'clip')
+      .append('rect')
+      .attr('width', this.focus.width)
+      .attr('height', this.focus.height);
+  }
+
+  /**
+   * Sets up scales, axes, and lines for both charts
+   */
+  setupCharts() {
     this.context.xScale = d3.scaleLinear().range([0, this.context.width]);
     this.focus.xScale   = d3.scaleLinear().range([0, this.focus.width]);
     this.context.yScale = d3.scaleLinear().range([this.context.height, 0]);
@@ -137,12 +174,6 @@ export class VisualisationViewerComponent implements OnInit {
       .x((d: any) => this.context.xScale(d[0]))
       .y((d: any) => this.context.yScale(d[1]));
 
-    this.svg.append('defs').append('clipPath')
-      .attr('id', 'clip')
-      .append('rect')
-      .attr('width', this.focus.width)
-      .attr('height', this.focus.height);
-
     this.focus.chartArea = this.svg.append('g')
       .attr('class', 'focus')
       .attr('transform', `translate(${this.focus.margin.left}, ${this.focus.margin.top})`);
@@ -155,7 +186,12 @@ export class VisualisationViewerComponent implements OnInit {
     this.focus.yScale.domain([d3.min(this.sin, d => d[1]), d3.max(this.sin, d => d[1])]);
     this.context.xScale.domain(this.focus.xScale.domain());
     this.context.yScale.domain(this.focus.yScale.domain());
+  }
 
+  /**
+   * Sets up the brush and zoom for the charts
+   */
+  setupBrushZoom() {
     this.brush = d3.brushX()
       .extent([[0, 0], [this.context.width, this.context.height]])
       .on('brush end', this.brushed.bind(this));
@@ -165,7 +201,12 @@ export class VisualisationViewerComponent implements OnInit {
       .translateExtent([[0, 0], [this.focus.width, this.focus.height]])
       .extent([[0, 0], [this.focus.width, this.focus.height]])
       .on('zoom', this.zoomed.bind(this));
+  }
 
+  /**
+   * Draws the band regions onto the focus chart
+   */
+  drawRegions() {
     this.regionColors = d3.scaleLinear().domain([0, this.regions.length]).range(<any[]>[
       'limegreen',
       'steelblue'
@@ -180,21 +221,16 @@ export class VisualisationViewerComponent implements OnInit {
         .style('fill', d => this.regionColors(i))
         .style('opacity', '0.3')
     }
+  }
 
-    this.focus.chartArea.append('path')
-      .datum(this.sin)
-      .attr('class', 'line')
-      .attr('d', this.focus.line);
-
+  /**
+   * Draws the context chart axis, line, and adds the brush
+   */
+  drawContextChart() {
     this.context.chartArea.append('path')
       .datum(this.sin)
       .attr('class', 'line')
       .attr('d', this.context.line);
-
-    this.focus.chartArea.append('g')
-      .attr('class', 'axis axis-x')
-      .attr('transform', `translate(0, ${this.focus.height})`)
-      .call(this.focus.xAxis);
 
     this.context.chartArea.append('g')
       .attr('class', 'axis axis-x')
@@ -207,6 +243,24 @@ export class VisualisationViewerComponent implements OnInit {
       .call(this.brush.move, this.context.xScale.range());
   }
 
+  /**
+   * Draws the focus chart and axis
+   */
+  drawFocusChart() {
+    this.focus.chartArea.append('path')
+      .datum(this.sin)
+      .attr('class', 'line')
+      .attr('d', this.focus.line);
+
+    this.focus.chartArea.append('g')
+      .attr('class', 'axis axis-x')
+      .attr('transform', `translate(0, ${this.focus.height})`)
+      .call(this.focus.xAxis);
+  }
+
+  /**
+   * Called when the brush is moved on the context chart
+   */
   brushed() {
     if (d3.event.sourceEvent && d3.event.sourceEvent.type === 'zoom') return;
     const s = d3.event.selection || this.context.xScale.range();
@@ -225,6 +279,9 @@ export class VisualisationViewerComponent implements OnInit {
       .translate(-s[0], 0));
   }
 
+  /**
+   * Called when the focus is zoomed
+   */
   zoomed() {
     if (d3.event.sourceEvent && d3.event.sourceEvent.type === 'brush') return;
     const t = d3.event.transform;
@@ -233,20 +290,6 @@ export class VisualisationViewerComponent implements OnInit {
     console.log(this.focus.chartArea.selectAll('.rect'));
     this.focus.chartArea.select('.axis-x').call(this.focus.xAxis);
     this.context.chartArea.select('.brush').call(this.brush.move, this.focus.xScale.range().map(t.invertX, t));
-  }
-
-  /**
-   * Creates the data array for the charts to use
-   */
-  createData() {
-    this.sin = [];
-    this.cos = [];
-    this.tan = [];
-    for (let i = 0; i < 1000; i++) {
-      this.sin.push([i, Math.sin(i / 10)]);
-      this.cos.push([i, Math.cos(i / 20)]);
-      this.tan.push([i, Math.tan(i / 10)]);
-    }
   }
 
   /**

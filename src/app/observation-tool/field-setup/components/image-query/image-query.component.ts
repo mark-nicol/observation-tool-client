ff63e893efb14587a9115bb7ae7bbf5289ae6201
JoeBarrett <joe.barrett@stfc.ac.uk>
2017-12-08 14:13:50 +0000

    Model based image query

--- src/app/observation-tool/field-setup/components/image-query/image-query.component.ts
+++ src/app/observation-tool/field-setup/components/image-query/image-query.component.ts
@@ -1,4 +1,7 @@
 import {Component} from '@angular/core';
+import {CURRENT_GOAL} from '../../../shared/data/current-science-goal';
+import {ImageQueryInterface} from '../../../shared/interfaces/science-goal-interfaces/field-setup-interfaces/image-query.interface';
+import {PersistenceService} from '../../../shared/services/persistence.service';
 
 /**
  * Image query component
@@ -12,5 +15,14 @@ import {Component} from '@angular/core';
   styleUrls: ['./image-query.component.css']
 })
 export class ImageQueryComponent {
+
+  data: ImageQueryInterface;
+
+  constructor(private persistenceService: PersistenceService) {}
+
+  ngOnInit() {
+    this.persistenceService.getScienceGoalPageSection(CURRENT_GOAL, 'fieldSetup', 'imageQuery')
+      .subscribe(res => this.data = res);
+  }
 
 }

0ab9945616aa6224e8260372ae69d51b5d12c5cf
JoeBarrett <joe.barrett@stfc.ac.uk>
2017-12-08 14:39:50 +0000

    Model based expected source properties

---
+++ src/app/observation-tool/field-setup/components/expected-source-properties/expected-source-properties.component.ts
@@ -0,0 +1,28 @@
+import {Component, OnInit} from '@angular/core';
+import {CURRENT_GOAL} from '../../../shared/data/current-science-goal';
+import {ExpectedSourcePropertiesInterface} from '../../../shared/interfaces/science-goal-interfaces/field-setup-interfaces/expected-source-properties.interface';
+import {PersistenceService} from '../../../shared/services/persistence.service';
+
+/**
+ * Source Expected properties component
+ */
+
+@Component({
+  selector: 'source-expected-properties',
+  templateUrl: './expected-source-properties.component.html',
+  styleUrls: ['./expected-source-properties.component.css']
+})
+export class ExpectedSourcePropertiesComponent implements OnInit{
+
+  data: ExpectedSourcePropertiesInterface;
+
+  constructor(private persistenceService: PersistenceService) {
+
+  }
+
+  ngOnInit() {
+    this.persistenceService.getSource(CURRENT_GOAL, 0)
+      .subscribe(res => this.data = res.expectedSourceProperties);
+  }
+
+}
--- src/app/observation-tool/field-setup/field-setup.module.ts
+++ src/app/observation-tool/field-setup/field-setup.module.ts
@@ -6,7 +6,7 @@ import {FieldCentreCoordinatesModule} from '../field-centre-coordinates/field-ce
 import {SharedModule} from '../shared/shared.module';
 import {FovParametersComponent} from './components/fov-parameters/fov-parameters.component';
 import {ImageQueryComponent} from './components/image-query/image-query.component';
-import {SourceExpectedPropertiesComponent} from './components/source-expected-properties/source-expected-properties.component';
+import {ExpectedSourcePropertiesComponent} from './components/expected-source-properties/expected-source-properties.component';
 import {SourceComponent} from './components/source/source.component';
 import {SpacialImageComponent} from './components/spacial-image/spacial-image.component';
 import {FieldSetupComponent} from './field-setup.component';
@@ -23,7 +23,7 @@ import {FieldSetupComponent} from './field-setup.component';
     FieldSetupComponent,
     FovParametersComponent,
     ImageQueryComponent,
-    SourceExpectedPropertiesComponent,
+    ExpectedSourcePropertiesComponent,
     SourceComponent,
     SpacialImageComponent
   ],

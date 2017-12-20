05b88999839aa0a8a0a9249b24417cbb23399fab
JoeBarrett <joe.barrett@stfc.ac.uk>
2017-12-20 13:32:58 +0000

    Remove system selector

--- src/app/observation-tool/field-setup/components/source/source.component.ts
+++ src/app/observation-tool/field-setup/components/source/source.component.ts
@@ -45,6 +45,97 @@ export class SourceComponent implements OnInit {
     'Ephemeris'
   ];
 
+  /** Iterator for the systems */
+  systemKeys: (o) => string[] = Object.keys;
+
+  /** Possible system choices dict */
+  systems: { [id: string]: CoordSystemInterface } = {
+    'icrs': {
+      sexagesimalLabels: {
+        latLabel: 'Dec',
+        lonLabel: 'RA',
+      },
+      normalLabels: {
+        latLabel: 'Dec(deg)',
+        lonLabel: 'RA(deg)',
+      },
+      latPlaceholder: '0',
+      lonPlaceholder: '0',
+      lonHeader: 'RA',
+      latHeader: 'Dec'
+    },
+    'FK5 J2000': {
+      sexagesimalLabels: {
+        latLabel: 'Dec',
+        lonLabel: 'RA',
+      },
+      normalLabels: {
+        latLabel: 'Dec(deg)',
+        lonLabel: 'RA(deg)'
+      },
+      latPlaceholder: '0',
+      lonPlaceholder: '0',
+      lonHeader: 'RA',
+      latHeader: 'Dec'
+    },
+    'galactic': {
+      sexagesimalLabels: {
+        latLabel: '',
+        lonLabel: '',
+      },
+      normalLabels: {
+        latLabel: 'Lat(deg)',
+        lonLabel: 'Lon(deg)',
+      },
+      latPlaceholder: '-60.18855219',
+      lonPlaceholder: '96.33728304',
+      lonHeader: 'Lon',
+      latHeader: 'Lat',
+    },
+    'eliptic': {
+      sexagesimalLabels: {
+        latLabel: '',
+        lonLabel: '',
+      },
+      normalLabels: {
+        latLabel: 'Lat (deg)',
+        lonLabel: 'Lon (deg)',
+      },
+      latPlaceholder: '0',
+      lonPlaceholder: '0',
+      lonHeader: 'RA',
+      latHeader: 'Deg'
+    },
+    'horizon': {
+      sexagesimalLabels: {
+        latLabel: '',
+        lonLabel: '',
+      },
+      normalLabels: {
+        latLabel: 'Alt(deg)',
+        lonLabel: 'Az(deg)',
+      },
+      latPlaceholder: '0',
+      lonPlaceholder: '0',
+      lonHeader: 'RA',
+      latHeader: 'Deg'
+    },
+    'azel': {
+      sexagesimalLabels: {
+        latLabel: '',
+        lonLabel: '',
+      },
+      normalLabels: {
+        latLabel: 'Alt(deg)',
+        lonLabel: 'Az(deg)',
+      },
+      latPlaceholder: '0',
+      lonPlaceholder: '0',
+      lonHeader: 'RA',
+      latHeader: 'Deg'
+    }
+  };
+
   /**
    * Retrieves data from service
    * @param persistenceService Injected service
@@ -59,13 +150,14 @@ export class SourceComponent implements OnInit {
                                                radialVelocityReferenceFrame: '',
                                                dopplerType: '',
                                                sexagesimalUnits: true,
-                                               redshift: 0
+                                               redshift: 0,
+                                               chosenSystem: ''
                                              })
   }
 
   ngOnInit() {
     this.persistenceService.getScienceGoal(CURRENT_PROJECT, CURRENT_SCIENCE_GOAL)
-        .subscribe(result => this.sourceForm.patchValue(result.fieldSetup.sources[0]));
+        .subscribe(result => this.sourceForm.setValue(result.fieldSetup.sources[0]));
   }
 
   /**

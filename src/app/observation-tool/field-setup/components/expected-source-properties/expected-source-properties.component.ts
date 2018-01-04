ce971939494d69aa3fcea22a7b2c1cdd103cb9b2
JoeBarrett <joe.barrett@stfc.ac.uk>
2018-01-04 12:07:52 +0000

    Add reactive form to expected source properties component

--- src/app/observation-tool/field-setup/components/expected-source-properties/expected-source-properties.component.ts
+++ src/app/observation-tool/field-setup/components/expected-source-properties/expected-source-properties.component.ts
@@ -1,4 +1,8 @@
 import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup} from '@angular/forms';
+import {CURRENT_PROJECT} from '../../../shared/data/current-project';
+import {CURRENT_SCIENCE_GOAL} from '../../../shared/data/current-science-goal';
+import {CURRENT_SOURCE} from '../../../shared/data/current-source';
 import {ExpectedSourcePropertiesInterface} from '../../../shared/interfaces/science-goal-interfaces/field-setup-interfaces/expected-source-properties.interface';
 import {PersistenceService} from '../../../shared/services/persistence.service';
 
@@ -7,21 +11,43 @@ import {PersistenceService} from '../../../shared/services/persistence.service';
  */
 
 @Component({
-  selector: 'source-expected-properties',
-  templateUrl: './expected-source-properties.component.html',
-  styleUrls: ['./expected-source-properties.component.css']
-})
-export class ExpectedSourcePropertiesComponent implements OnInit{
+             selector: 'source-expected-properties',
+             templateUrl: './expected-source-properties.component.html',
+             styleUrls: ['./expected-source-properties.component.css']
+           })
+export class ExpectedSourcePropertiesComponent implements OnInit {
 
   data: ExpectedSourcePropertiesInterface;
+  expectedSourcePropertiesForm: FormGroup;
 
-  constructor(private persistenceService: PersistenceService) {
-
+  constructor(private persistenceService: PersistenceService, private formBuilder: FormBuilder) {
+    this.expectedSourcePropertiesForm = this.formBuilder.group({
+                                                                 continuumFluxDensityUnit: '',
+                                                                 continuumFluxDensityValue: 0.0,
+                                                                 continuumPolarization: 0.0,
+                                                                 lineFluxDensityUnit: '',
+                                                                 lineFluxDensityValue: 0.0,
+                                                                 lineWidthUnit: '',
+                                                                 lineWidthValue: 0.0,
+                                                                 linePolarization: 0.0
+                                                               })
   }
 
   ngOnInit() {
-    // this.persistenceService.getSource(CURRENT_GOAL, 0)
-    //   .subscribe(res => this.data = res.expectedSourceProperties);
+    this.persistenceService.getSource(CURRENT_PROJECT, CURRENT_SCIENCE_GOAL, CURRENT_SOURCE)
+        .subscribe(res => {
+          const esp = res.expectedSourceProperties;
+          this.expectedSourcePropertiesForm.setValue({
+                                                       continuumFluxDensityUnit: esp.continuumFluxDensity.unit,
+                                                       continuumFluxDensityValue: esp.continuumFluxDensity.value,
+                                                       continuumPolarization: esp.continuumPolarization,
+                                                       lineFluxDensityUnit: esp.lineFluxDensity.unit,
+                                                       lineFluxDensityValue: esp.lineFluxDensity.value,
+                                                       lineWidthUnit: esp.lineWidth.unit,
+                                                       lineWidthValue: esp.lineWidth.value,
+                                                       linePolarization: esp.linePolarization
+                                                     })
+        });
   }
 
 }

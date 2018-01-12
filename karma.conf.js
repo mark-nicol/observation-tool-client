a02afb8591d4ef42c6eb827237eb2615023ef691
JoeBarrett <joe.barrett@stfc.ac.uk>
2018-01-12 14:24:29 +0000

    Revert to f4ff6863

--- karma.conf.js
+++ karma.conf.js
@@ -1,8 +1,6 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/0.13/config/configuration-file.html
 
-process.env.CHROME_BIN = require('puppeteer').executablePath();
-
 module.exports = function (config) {
   config.set({
     basePath: '',
@@ -35,18 +33,17 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
+    browsers: [
+      'Chrome',
+      // 'PhantomJS'
+      // 'IE',
+      // 'Firefox',
+      // 'FirefoxDeveloper',
+      // 'FirefoxAurora',
+      // 'FirefoxNightly',
+      // 'Safari'
+    ],
     singleRun: false,
-    watch: false,
-    browsers: ['ChromeHeadlessNoSandbox'],
-    customLaunchers: {
-      ChromeHeadlessNoSandbox: {
-        base: 'ChromeHeadless',
-        flags: [
-          '--no-sandbox', // required to run without privileges in docker
-          '--user-data-dir=/tmp/chrome-test-profile',
-          '--disable-web-security'
-        ]
-      }
-    }
+    watch: false
   });
 };

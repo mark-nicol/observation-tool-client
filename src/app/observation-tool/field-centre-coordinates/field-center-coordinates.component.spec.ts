d5ef9b37834a2a0170d5c430858e2453bf09dcfe
JoeBarrett <joe.barrett@stfc.ac.uk>
2018-02-08 16:20:00 +0000

    Test fixes

--- src/app/observation-tool/field-centre-coordinates/field-center-coordinates.component.spec.ts
+++ src/app/observation-tool/field-centre-coordinates/field-center-coordinates.component.spec.ts
@@ -1,6 +1,7 @@
 import {HttpClientTestingModule} from '@angular/common/http/testing';
 import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 import {ReactiveFormsModule} from '@angular/forms';
+import {RouterTestingModule} from '@angular/router/testing';
 import {SuiModule} from 'ng2-semantic-ui';
 import {DegreesPipe} from '../shared/pipes/degrees.pipe';
 import {SexagesimalPipe} from '../shared/pipes/sexagesimal.pipe';
@@ -27,7 +28,8 @@ describe('FieldCenterCoordinatesComponent', () => {
                                      imports: [
                                        ReactiveFormsModule,
                                        SuiModule,
-                                       HttpClientTestingModule
+                                       HttpClientTestingModule,
+                                       RouterTestingModule
                                      ],
                                      providers: [
                                        PersistenceService,

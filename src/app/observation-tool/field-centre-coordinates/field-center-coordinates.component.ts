aec1ef83a0d22bedb589a26c752debbe7b827965
JoeBarrett <joe.barrett@stfc.ac.uk>
2018-02-08 16:17:56 +0000

    ESP and FCC components working with multiple sources

--- src/app/observation-tool/field-centre-coordinates/field-center-coordinates.component.ts
+++ src/app/observation-tool/field-centre-coordinates/field-center-coordinates.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup} from '@angular/forms';
+import {ActivatedRoute} from '@angular/router';
 import {ISinglePoint} from '../shared/interfaces/project/science-goal/target-parameters.interface';
 import {PersistenceService} from '../shared/services/persistence.service';
 
@@ -8,15 +9,16 @@ import {PersistenceService} from '../shared/services/persistence.service';
  */
 
 @Component({
-             selector:    'field-center-coordinates',
-             templateUrl: './field-center-coordinates.component.html',
-             styleUrls:   ['./field-center-coordinates.component.css'],
-           })
+  selector: 'field-center-coordinates',
+  templateUrl: './field-center-coordinates.component.html',
+  styleUrls: ['./field-center-coordinates.component.css'],
+})
 export class FieldCenterCoordinatesComponent implements OnInit {
 
   fieldCentreCoordinatesForm: FormGroup;
   tableRows: ISinglePoint[];
   sourceCoordinatesSystem = 'ICRS';
+  currentTarget           = 0;
 
   /** Field Setup Service to be used in HTML template */
   protected _persistenceService: PersistenceService;
@@ -25,96 +27,83 @@ export class FieldCenterCoordinatesComponent implements OnInit {
    * Sets _fieldSetupService and retrieves page data
    * @param persistenceService Injected service
    * @param formBuilder
+   * @param activatedRoute
    */
-  constructor(private persistenceService: PersistenceService, private formBuilder: FormBuilder) {
+  constructor(private persistenceService: PersistenceService,
+              private formBuilder: FormBuilder,
+              private activatedRoute: ActivatedRoute) {
     this.fieldCentreCoordinatesForm = formBuilder.group({
-                                                          coordType:   '',
-                                                          targetType:  '',
-                                                          individual:  formBuilder.group({
-                                                                                           offsetUnit: '',
-                                                                                           rows:       this.formBuilder.array([])
-                                                                                         }),
-                                                          rectangular: formBuilder.group({
-                                                                                           chosenSystem:       '',
-                                                                                           sexagesimalUnits:   false,
-                                                                                           lonOffsetUnit:      '',
-                                                                                           lonOffsetValue:     0.0,
-                                                                                           latOffsetUnit:      '',
-                                                                                           latOffsetValue:     0.0,
-                                                                                           pLengthUnit:        '',
-                                                                                           pLengthValue:       0.0,
-                                                                                           qLengthUnit:        '',
-                                                                                           qLengthValue:       0.0,
-                                                                                           positionAngleUnit:  '',
-                                                                                           positionAngleValue: 0.0,
-                                                                                           spacingValue:       0.0,
-                                                                                           spacingUnits:       ''
-                                                                                         })
-                                                        });
+      coordType: '',
+      targetType: '',
+      individual: formBuilder.group({
+        offsetUnit: '',
+        rows: this.formBuilder.array([])
+      }),
+      rectangular: formBuilder.group({
+        chosenSystem: '',
+        sexagesimalUnits: false,
+        lonOffsetUnit: '',
+        lonOffsetValue: 0.0,
+        latOffsetUnit: '',
+        latOffsetValue: 0.0,
+        pLengthUnit: '',
+        pLengthValue: 0.0,
+        qLengthUnit: '',
+        qLengthValue: 0.0,
+        positionAngleUnit: '',
+        positionAngleValue: 0.0,
+        spacingValue: 0.0,
+        spacingUnits: ''
+      })
+    });
   }
 
   ngOnInit() {
+    this.activatedRoute.params.subscribe(params => {
+      this.currentTarget = params.index;
+      this.initComponent(this.currentTarget);
+    });
+    this.initComponent(this.currentTarget);
+  }
+
+  initComponent(targetIndex: number) {
     this.persistenceService.getScienceGoal()
         .subscribe(result => {
-          this.sourceCoordinatesSystem = result.TargetParameters[this.persistenceService.currentTarget].sourceCoordinates.system;
-          this.fieldCentreCoordinatesForm.patchValue({targetType: result.TargetParameters[this.persistenceService.currentTarget].type});
-          if (result.TargetParameters[this.persistenceService.currentTarget].type === 'F_MultiplePoints') { // TODO Enum
+          this.sourceCoordinatesSystem = result.TargetParameters[targetIndex].sourceCoordinates.system;
+          this.fieldCentreCoordinatesForm.patchValue({targetType: result.TargetParameters[targetIndex].type});
+          if (result.TargetParameters[targetIndex].type === 'F_MultiplePoints') { // TODO Enum
             this.fieldCentreCoordinatesForm.patchValue({
-                                                         coordType:  result.TargetParameters[this.persistenceService.currentTarget].SinglePoint[0].centre.type,
-                                                         individual: {
-                                                           offsetUnit: result.TargetParameters[this.persistenceService.currentTarget].SinglePoint[0].centre.latitude.unit
-                                                         }
-                                                       });
-            this.tableRows = result.TargetParameters[this.persistenceService.currentTarget].SinglePoint;
+              coordType: result.TargetParameters[targetIndex].SinglePoint[0].centre.type,
+              individual: {
+                offsetUnit: result.TargetParameters[targetIndex].SinglePoint[0].centre.latitude.unit
+              }
+            });
+            this.tableRows = result.TargetParameters[targetIndex].SinglePoint;
           } else {
-            const rect = result.TargetParameters[this.persistenceService.currentTarget].Rectangle;
+            const rect = result.TargetParameters[targetIndex].Rectangle;
             console.log(rect.spacing);
             this.fieldCentreCoordinatesForm.patchValue({
-                                                         coordType:   rect.centre.type,
-                                                         rectangular: {
-                                                           chosenSystem:       rect.centre.system,
-                                                           sexagesimalUnits:   rect.centre.type === 'ABSOLUTE',
-                                                           lonOffsetUnit:      rect.centre.longitude.unit,
-                                                           lonOffsetValue:     rect.centre.longitude.content,
-                                                           latOffsetUnit:      rect.centre.latitude.unit,
-                                                           latOffsetValue:     rect.centre.latitude.content,
-                                                           pLengthUnit:        rect.long.unit,
-                                                           pLengthValue:       rect.long.content,
-                                                           qLengthUnit:        rect.short.unit,
-                                                           qLengthValue:       rect.short.content,
-                                                           positionAngleUnit:  rect.pALong.unit,
-                                                           positionAngleValue: rect.pALong.content,
-                                                           spacingValue:       rect.spacing.content,
-                                                           spacingUnits:       rect.spacing.userUnit
-                                                         }
-                                                       })
+              coordType: rect.centre.type,
+              rectangular: {
+                chosenSystem: rect.centre.system,
+                sexagesimalUnits: rect.centre.type === 'ABSOLUTE',
+                lonOffsetUnit: rect.centre.longitude.unit,
+                lonOffsetValue: rect.centre.longitude.content,
+                latOffsetUnit: rect.centre.latitude.unit,
+                latOffsetValue: rect.centre.latitude.content,
+                pLengthUnit: rect.long.unit,
+                pLengthValue: rect.long.content,
+                qLengthUnit: rect.short.unit,
+                qLengthValue: rect.short.content,
+                positionAngleUnit: rect.pALong.unit,
+                positionAngleValue: rect.pALong.content,
+                spacingValue: rect.spacing.content,
+                spacingUnits: rect.spacing.userUnit
+              }
+            })
           }
-          // this.tableRows = fcc.individual.rows;
-          // this.fieldCentreCoordinatesForm.patchValue({
-          // coordType: fcc.coordType,
-          // targetType: fcc.targetType,
-          //                                              individual: {
-          //                                                offsetUnit: fcc.individual.offsetUnit
-          //                                              },
-          //                                              rectangular: {
-          //                                                chosenSystem: fcc.rectangular.chosenSystem,
-          //                                                sexagesimalUnits: fcc.rectangular.sexagesimalUnits,
-          //                                                lonOffsetUnit: fcc.rectangular.lonOffset.unit,
-          //                                                lonOffsetValue: fcc.rectangular.lonOffset.content,
-          //                                                latOffsetUnit: fcc.rectangular.latOffset.unit,
-          //                                                latOffsetValue: fcc.rectangular.latOffset.content,
-          //                                                pLengthUnit: fcc.rectangular.pLength.unit,
-          //                                                pLengthValue: fcc.rectangular.pLength.content,
-          //                                                qLengthUnit: fcc.rectangular.qLength.unit,
-          //                                                qLengthValue: fcc.rectangular.qLength.content,
-          //                                                positionAngleUnit: fcc.rectangular.positionAngle.unit,
-          //                                                positionAngleValue: fcc.rectangular.positionAngle.content,
-          //                                                spacing: fcc.rectangular.spacing,
-          //                                                spacingUnits: fcc.rectangular.spacingUnits
-          //                                              }
-          //                                            });
 
-        })
+        });
   }
 
 }
--- src/app/observation-tool/field-setup/components/expected-source-properties/expected-source-properties.component.ts
+++ src/app/observation-tool/field-setup/components/expected-source-properties/expected-source-properties.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup} from '@angular/forms';
+import {ActivatedRoute} from '@angular/router';
 import {PersistenceService} from '../../../shared/services/persistence.service';
 
 /**
@@ -7,42 +8,52 @@ import {PersistenceService} from '../../../shared/services/persistence.service';
  */
 
 @Component({
-             selector: 'source-expected-properties',
-             templateUrl: './expected-source-properties.component.html',
-             styleUrls: ['./expected-source-properties.component.css']
-           })
+  selector: 'source-expected-properties',
+  templateUrl: './expected-source-properties.component.html',
+  styleUrls: ['./expected-source-properties.component.css']
+})
 export class ExpectedSourcePropertiesComponent implements OnInit {
 
-  data: any;
   expectedSourcePropertiesForm: FormGroup;
+  currentTarget = 0;
 
-  constructor(private persistenceService: PersistenceService, private formBuilder: FormBuilder) {
+  constructor(private persistenceService: PersistenceService,
+              private formBuilder: FormBuilder,
+              private activatedRoute: ActivatedRoute) {
     this.expectedSourcePropertiesForm = this.formBuilder.group({
-                                                                 continuumFluxDensityUnit: '',
-                                                                 continuumFluxDensityValue: 0.0,
-                                                                 continuumPolarization: 0.0,
-                                                                 lineFluxDensityUnit: '',
-                                                                 lineFluxDensityValue: 0.0,
-                                                                 lineWidthUnit: '',
-                                                                 lineWidthValue: 0.0,
-                                                                 linePolarization: 0.0
-                                                               })
+      continuumFluxDensityUnit: '',
+      continuumFluxDensityValue: 0.0,
+      continuumPolarization: 0.0,
+      lineFluxDensityUnit: '',
+      lineFluxDensityValue: 0.0,
+      lineWidthUnit: '',
+      lineWidthValue: 0.0,
+      linePolarization: 0.0
+    })
   }
 
   ngOnInit() {
+    this.activatedRoute.params.subscribe(params => {
+      this.currentTarget = params.index;
+      this.initComponent(this.currentTarget);
+    });
+    this.initComponent(this.currentTarget);
+  }
+
+  initComponent(targetIndex: number) {
     this.persistenceService.getScienceGoal()
         .subscribe(result => {
-          const esp = result.TargetParameters[this.persistenceService.currentTarget].ExpectedProperties;
+          const esp = result.TargetParameters[targetIndex].ExpectedProperties;
           this.expectedSourcePropertiesForm.patchValue({
-                                                       continuumFluxDensityUnit: esp.expectedPeakFluxDensity.unit,
-                                                       continuumFluxDensityValue: esp.expectedPeakFluxDensity.content,
-                                                       continuumPolarization: esp.desiredCircularPolarizationPercentage,
-                                                       lineFluxDensityUnit: esp.expectedPeakLineFluxDensity.unit,
-                                                       lineFluxDensityValue: esp.expectedPeakLineFluxDensity.content,
-                                                       lineWidthUnit: esp.expectedLineWidth.userUnit,
-                                                       lineWidthValue: esp.expectedLineWidth.content,
-                                                       linePolarization: esp.desiredLinePolarizationPercentage
-                                                     })
+            continuumFluxDensityUnit: esp.expectedPeakFluxDensity.unit,
+            continuumFluxDensityValue: esp.expectedPeakFluxDensity.content,
+            continuumPolarization: esp.desiredCircularPolarizationPercentage,
+            lineFluxDensityUnit: esp.expectedPeakLineFluxDensity.unit,
+            lineFluxDensityValue: esp.expectedPeakLineFluxDensity.content,
+            lineWidthUnit: esp.expectedLineWidth.userUnit,
+            lineWidthValue: esp.expectedLineWidth.content,
+            linePolarization: esp.desiredLinePolarizationPercentage
+          })
         });
   }
 

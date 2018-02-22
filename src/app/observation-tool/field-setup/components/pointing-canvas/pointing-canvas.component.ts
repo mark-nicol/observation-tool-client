aa2e6e4d8e3aa62ad533797b6cb43846834b15a5
JoeBarrett <joe.barrett@stfc.ac.uk>
2018-02-22 13:26:51 +0000

    Component set up

--- src/app/observation-tool/field-setup/components/pointing-canvas/pointing-canvas.component.ts
+++ src/app/observation-tool/field-setup/components/pointing-canvas/pointing-canvas.component.ts
@@ -1,4 +1,4 @@
-import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-pointing-canvas',
@@ -7,14 +7,10 @@ import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 })
 export class PointingCanvasComponent implements OnInit {
 
-  @ViewChild('pointingCanvas') canvasRef: ElementRef;
-
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
-    const ctx: CanvasRenderingContext2D = this.canvasRef.nativeElement.getContext('2d');
-    ctx.strokeStyle = '#FF0000';
-    ctx.strokeRect(50, 50, 50, 50);
   }
 
 }

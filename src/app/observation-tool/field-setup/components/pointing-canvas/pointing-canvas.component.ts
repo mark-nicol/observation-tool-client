e53e2c701117252c14312d314d3b4f0c05fda702
JoeBarrett <joe.barrett@stfc.ac.uk>
2018-02-22 11:13:00 +0000

    Add rect to canvas

--- src/app/observation-tool/field-setup/components/pointing-canvas/pointing-canvas.component.ts
+++ src/app/observation-tool/field-setup/components/pointing-canvas/pointing-canvas.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-pointing-canvas',
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PointingCanvasComponent implements OnInit {
 
+  @ViewChild('pointingCanvas') canvasRef: ElementRef;
+
   constructor() { }
 
   ngOnInit() {
+    const ctx: CanvasRenderingContext2D = this.canvasRef.nativeElement.getContext('2d');
+    ctx.strokeStyle = '#FF0000';
+    ctx.strokeRect(50, 50, 50, 50);
   }
 
 }

e6922ffec9a9af94e6dda3a0f671a6c5a57eeca5
JoeBarrett <joe.barrett@stfc.ac.uk>
2018-04-06 12:24:37 +0100

    Improve modal styling

--- src/app/observation-tool/shared/components/project-import-modal/project-import-modal.component.ts
+++ src/app/observation-tool/shared/components/project-import-modal/project-import-modal.component.ts
@@ -26,7 +26,7 @@ export class ProjectImportModal extends ComponentModalConfig<ModalContext, void,
     super(ProjectImportModalComponent, {name, title});
     this.transitionDuration = 200;
     this.size = size;
-    this.mustScroll = false;
+    this.mustScroll = true;
     this.isClosable = true;
   }
 

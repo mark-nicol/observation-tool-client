3f8e7ea9666b5b7646e619adebe29ac8454079c8
JoeBarrett <joe.barrett@stfc.ac.uk>
2018-04-12 10:44:59 +0100

    Add start component

--- src/app/observation-tool/project/project-routing.module.ts
+++ src/app/observation-tool/project/project-routing.module.ts
@@ -4,11 +4,16 @@ import {PiEntryComponent} from './components/pi-entry/pi-entry.component';
 import {PlannedObservingComponent} from './components/planned-observing/planned-observing.component';
 import {ProposalComponent} from './components/proposal/proposal.component';
 import {ProjectImportComponent} from '../shared/components/project-import/project-import.component';
+import {StartComponent} from './components/start/start.component';
 
 const routes: Routes = [
   {
     path:     'project',
     children: [
+      {
+        path: 'new-start',
+        component: StartComponent
+      },
       {
         path:      'pi-entry',
         component: PiEntryComponent
@@ -23,7 +28,7 @@ const routes: Routes = [
       },
       {
         path:       '',
-        redirectTo: 'pi-entry',
+        redirectTo: 'new-start',
         pathMatch:  'full'
       }
     ],

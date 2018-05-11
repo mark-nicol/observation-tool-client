f879d74cb9e97f6d3b9fe5614853794948bc7bf5
JoeBarrett <joe.barrett@stfc.ac.uk>
2018-05-11 13:04:06 +0100

    Strip out functionality

--- src/app/observation-tool/project/components/pi-entry/pi-search/pi-search.component.ts
+++ src/app/observation-tool/project/components/pi-entry/pi-search/pi-search.component.ts
@@ -3,7 +3,6 @@ import {SuiModalService} from 'ng2-semantic-ui';
 import {ToastsManager} from 'ng2-toastr';
 import {AlmaInvestigatorSearchModal} from '../../../../alma-investigator-search/components/modal/modal.component';
 import {AlmaInvestigatorSearchService} from '../../../../alma-investigator-search/services/alma-investigator-search.service';
-import {IAlmaInvestigator} from '../../../../shared/interfaces/alma-investigator.interface';
 import {ProjectService} from '../../../../shared/services/project.service';
 
 /**
@@ -21,43 +20,21 @@ export class PiSearchComponent implements OnInit {
   /** Placeholder for the search input */
   INPUT_PLACEHOLDER = 'Enter Principal Investigator name';
 
-  /** The chosen PI passed back from piSelect */
-  passedPi: IAlmaInvestigator;
-
-  isLoading = true;
+  isLoading;
 
   constructor(private almaInvestigatorSearchService: AlmaInvestigatorSearchService,
-              private persistenceService: ProjectService,
+              private projectService: ProjectService,
               private suiModalService: SuiModalService,
               private toastMgr: ToastsManager,
               viewContainerRef: ViewContainerRef) {
     this.toastMgr.setRootViewContainerRef(viewContainerRef);
-    this.persistenceService.loadedProject.subscribe(result => {
-      if (result) {
-        this.isLoading = true;
-        this.passedPi = null;
-        this.almaInvestigatorSearchService.search('Uid', result.prj_pI).subscribe(
-          res => this.passedPi = res[0],
-          error => {
-          },
-          complete => this.isLoading = false);
-      }
-    });
   }
 
   /**
    * Checks for a chosen PI in session storage and sets if available
    */
   ngOnInit() {
-    if (sessionStorage['selectedPi']) {
-      this.passedPi = JSON.parse(sessionStorage.getItem('selectedPi'));
-    }
-  }
 
-  newPi() {
-    if (sessionStorage['selectedPi']) {
-      this.passedPi = JSON.parse(sessionStorage.getItem('selectedPi'));
-    }
   }
 
   makeModal(piName: string) {
@@ -65,10 +42,7 @@ export class PiSearchComponent implements OnInit {
       .open(new AlmaInvestigatorSearchModal(piName))
       .onApprove(result => console.log(result)/*this.newPi()*/)
       .onDeny(result => {
-        if (sessionStorage.getItem('modalError') === 'unknown error') {
-          this.toastMgr.error('Could not contact user lookup server', 'Error');
-          sessionStorage.removeItem('modalError');
-        }
+
       });
   }
 }

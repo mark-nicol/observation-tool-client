744a74eb4e9fa8946c26d996a51bea75e0c4bbfc
JoeBarrett <joe.barrett@stfc.ac.uk>
2018-05-24 17:17:10 +0100

    Fix fcc component

--- src/app/observation-tool/shared/interfaces/apdm/proposal-feedback.interface.ts
+++ src/app/observation-tool/shared/interfaces/apdm/proposal-feedback.interface.ts
@@ -1,9 +1,8 @@
-import {
-  IArrayTimeBreakdown} from './obs-proposal.interface';
 import {Time} from '../../../../units/classes/time';
 import {IDataRateBreakdown} from './data-rate-breakdown.interface';
 import {IReceiverTimeBreakdown} from './receiver-time-breakdown.interface';
 import {ITimeAllocationBreakdown} from './time-allocation-breakdown.interface';
+import {IArrayTimeBreakdown} from './array-time-breakdown.interface';
 
 export interface IProposalFeedback {
   estimatedTotalIntegrationTime: Time;
